ad42a0ffadfcaafeef1baa45bdd94a2eb8f94b95
lapeko <lapeko88@gmail.com>
2024-08-08 16:48:36 +0200

    implement theme changing

---
+++ angular-material-theme-changing/.angular/cache/18.1.4/vite/deps/@angular_cdk_layout.js
@@ -0,0 +1,16 @@
+import {
+  BreakpointObserver,
+  Breakpoints,
+  LayoutModule,
+  MediaMatcher
+} from "./chunk-RPZUIV7B.js";
+import "./chunk-Y2DOBZ6Q.js";
+import "./chunk-HDSHWE5H.js";
+import "./chunk-M5HG5EZS.js";
+export {
+  BreakpointObserver,
+  Breakpoints,
+  LayoutModule,
+  MediaMatcher
+};
+//# sourceMappingURL=@angular_cdk_layout.js.map
--- angular-material-theme-changing/src/app/app.component.ts
+++ angular-material-theme-changing/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, Renderer2} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavigationComponent} from "./navigation/navigation.component";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-material-theme-cahnging';
+export class AppComponent implements OnInit {
+  #renderer = inject(Renderer2);
+  #document = inject(DOCUMENT);
+
+  ngOnInit() {
+    this.#renderer.addClass(this.#document.body, "light-theme");
+  }
+
+
+  onThemeChanged(isDark: boolean) {
+    this.#renderer.removeClass(this.#document.body, `${isDark ? 'light' : 'dark'}-theme`);
+    this.#renderer.addClass(this.#document.body, `${isDark ? 'dark' : 'light'}-theme`);
+  }
 }
